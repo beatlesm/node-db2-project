6d1a74e8d480eb4cf2a48817575fb704c690194c
beatlesm <rj.geng@gmail.com>
2022-02-15 15:43:48 -0800

    Task 2: Cars seed created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,9 @@
-// STRETCH
+exports.seed = function (knex) {
+    return knex('cars').truncate()
+      .then(function () {
+        return knex('cars').insert([
+          { vin: '1FTYR14U72A67071', make: 'FORD', model: 'RANGE', mileage: 170000, title: 'clean', transmission: 'automatic' },
+          
+        ]);
+      });
+  };
